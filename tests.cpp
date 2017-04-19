5ddfbf657195aaf6b7f28bd0984ceb93b756e1e3
henry kwak <henry.kwak@obigo.com>
2017-04-19 11:33:46 +0900

    good

--- tests.cpp
+++ tests.cpp
@@ -3,7 +3,7 @@
 #include <gtest/gtest.h>
 
 TEST(SquareRootTest2, PositiveNos) { 
-    EXPECT_EQ(8, squareRoot(36.0));
+    EXPECT_EQ(6, squareRoot(36.0));
     EXPECT_EQ(18.0, squareRoot(324.0));
     EXPECT_EQ(25.4, squareRoot(645.16));
     EXPECT_EQ(0, squareRoot(0.0));
