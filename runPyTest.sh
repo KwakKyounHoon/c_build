980161c8feca43ecabb8afbc08eb174015d5b72f
henry kwak <henry.kwak@obigo.com>
2017-04-24 15:18:28 +0900

    sh

--- runPyTest.sh
+++ runPyTest.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd pytest
-sudo swig -python pre_example.i
+swig -python pre_example.i
 gcc -c ../test_example.c pre_example_wrap.c -I/usr/include/python2.7
 ld -shared test_example.o pre_example_wrap.o -o _py_test_example.so
 pytest --junit-xml=../test/pytest.xml test.py 
