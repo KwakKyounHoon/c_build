499cb56518bfe42257bc075a7bf73ed799ac5198
henry kwak <henry.kwak@obigo.com>
2017-04-24 11:07:47 +0900

    collect test

--- runPyTest.sh
+++ runPyTest.sh
@@ -3,4 +3,4 @@ cd pytest
 swig -python pre_example.i
 gcc -c ../test_example.c pre_example_wrap.c -I/usr/include/python2.7
 ld -shared test_example.o pre_example_wrap.o -o _py_test_example.so
-pytest --junit-xml=pytest.xml test.py 
+pytest --junit-xml=../test/pytest.xml test.py 
